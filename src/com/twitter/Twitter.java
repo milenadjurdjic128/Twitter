90e7ccc55ee416c94ebe703021b254cb768a6459
Milena <Milena@milena-hp>
2015-04-07 15:09:38 +0200

    Dopisan kod za metodu tearDown

--- src/com/twitter/Twitter.java
+++ src/com/twitter/Twitter.java
@@ -2,14 +2,35 @@ package com.twitter;
 
 	import java.util.LinkedList; 
 	import com.twitter.poruke.TwitterPoruka;
-	
+	/**
+	 * Klasa Twitter sadrzi metode za vracanje svih poruka, unos nove poruke, za vracanje poruka
+	 * vezanih za odredjeni tag
+	 * 
+	 * @author Milena
+	 *
+	 */
 	public class Twitter { 
+		/**
+		 * Atribut poruke koji predstavlja listu svih poruka, inicijalizovana je lista.
+		 */
 		private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>(); 
 		
+		/**
+		 * Metoda vratiSvePoruke vraca sve poruke.
+		 * 
+		 * @return sve poruke iz liste
+		 */
 		public LinkedList<TwitterPoruka> vratiSvePoruke(){
 			return poruke;
 		}
 		
+		
+		/**
+		 * Metoda unosi novu poruku za korisnika.
+		 * 
+		 * @param korisnik koji unosi poruku
+		 * @param poruka uneta od strane odredjenog korisnika
+		 */
 		public void unesi(String korisnik, String poruka) { 
 			//Pravi se nova poruka i puni podacima.
 			TwitterPoruka tp = new TwitterPoruka(); 
@@ -20,6 +41,15 @@ package com.twitter;
 			//Poruka se unosi u listu na kraj 
 			poruke.addLast(tp); 
 		} 
+		
+		/**
+		 * Metoda vraca sve poruke vezane za jedan odredjeni tag, koji je unet kao parametar a broj poruka
+		 * je odredjen unetim maksimalnim brojem poruka koje mogu da prikazu, maxBroj.
+		 * 
+		 * @param maxBroj je maksimalan broj poruka koje je moguce prikazati odjednom
+		 * @param tag predstavlja vrednost na osnovu koje pretrazujemo poruke, sve one koje sadrze taj tag
+		 * @return lista poruka koje sadrze odredjeni tag
+		 */
 		public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) { 
 			 if (tag==null || tag.isEmpty())
 				throw new RuntimeException("Morate uneti tag");
@@ -31,7 +61,7 @@ package com.twitter;
 			//Pomocna promenljiva koja predstavlja brojac upisanih poruka 
 			int brojac = 0; 
 			
-			//Pomocni niz koja predstavlja rezultat pretrage tj. sadrzace
+			//Pomocni niz koji predstavlja rezultat pretrage tj. sadrzace
 			//sve poruke koje u sebi imaju zadati tag
 			TwitterPoruka[] rezultat = new TwitterPoruka[maxBroj]; 
 			
--- test/com/twitter/poruke/TwitterPorukaTest.java
+++ test/com/twitter/poruke/TwitterPorukaTest.java
@@ -17,6 +17,7 @@ public class TwitterPorukaTest {
 
 	@After
 	public void tearDown() throws Exception {
+		tweet = null;
 	}
 
 	@Test
